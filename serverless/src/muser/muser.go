dc84217d764881db10b743538c6e050912a59e34
olesiapoz <test@gitmail>
2018-12-25 16:05:16 +0200

    clean serverless file

--- serverless/src/muser/muser.go
+++ serverless/src/muser/muser.go
@@ -1,40 +1,28 @@
 package muser
 
 import (
-  "aaa"
-// "github.com/subosito/gotenv"
-	"crypto/sha1"
-//	"reflect"
+	"aaa"
+	//	"crypto/sha1"
 	corelog "log"
-	"fmt"
-	"io"
-//	"encoding/json"
-//	"os"
-  //    "log"
+	//	"encoding/json"
+	//	"os"
+	//    "log"
 	"github.com/microservices-demo/user/api"
 	"github.com/microservices-demo/user/db"
-	"github.com/microservices-demo/user/users"
 	"github.com/microservices-demo/user/db/mongodb"
-
-
-//	"github.com/olesiapoz/Serverless-microservices/user/db/mongodb"
-
+	"github.com/microservices-demo/user/users"
 )
 
 type userResponse struct {
-        User users.User `json:"user"`
+	User users.User `json:"user"`
 }
 
 func init() {
-aaa.Load()
-//fmt.Println(os.Getenv("USER_DATABASE"))
+	aaa.Load()
 }
 
 func init() {
 	db.Register("mongodb", &mongodb.Mongo{})
-//corelog.Print("DbInit")
-//	session := *mongodb.Mongo{}
-//	fmt.Println("Created DB")
 }
 
 /*
@@ -49,85 +37,39 @@ func main(){
 */
 
 func Muser(req map[string]interface{}) map[string]interface{} {
-//        os.Setenv("MONGO_HOST","192.168.0.3:27017")
-//        os.Setenv("USER_DATABASE", "mongodb")
-	fmt.Println(req) 
- 
+
 	password := req["password"].(string)
-	//password := string(passw[0:len(passw)-1])
 	username := req["user"].(string)
-    
-    dbconn := false
-    //    fmt.Println("Done")
+
+	//db connection
+	dbconn := false
+
 	for !dbconn {
 		err := db.Init()
-//		fmt.Println(db.DBTypes)
 		if err != nil {
 			if err == db.ErrNoDatabaseSelected {
 				corelog.Fatal(err)
 			}
-//			corelog.Print("DB : " + err.Error())
 		} else {
 			dbconn = true
-  //                      fmt.Println("Connected to DB")
 		}
 	}
-	
-        // Service domain.
+
+	// Service domain.
 	var service api.Service
 	{
 		service = api.NewFixedService()
 	}
-//      corelog.Print("service" + service.Login(username, password).user.id)
-
-	u, err := db.GetUserByName(username)
-	if err != nil {
-        corelog.Print("E1: " + err.Error())
-        fmt.Println("E2 : " + err.Error())
-        }
-
-	
-	salt := u.Salt
-
-        fmt.Println("Salt: " + salt)
-        fmt.Println("PSW: " + u.Password)
-	fmt.Println("user: " + username + " pwd: " + password)
-	fmt.Println(calculatePassHash(password, salt))
 
 	var user users.User
-	//corelog.Print("User" + user)	
+
 	user, err1 := service.Login(username, password)
 	if err1 != nil {
-	corelog.Print("R1: " + err1.Error())
-	fmt.Println("R2 : " + err1.Error())
+		corelog.Print("Login Service Error: " + err1.Error())
 	}
 
+	mp := make(map[string]interface{})
+	mp["user"] = user
 
-	
-//	json_st, _ := json.Marshal(userResponse{User: user})
-//	mp =: map[string]string(userResponse{User: user})
-//      json_st, _ := json.Marshal(mp)
-
-  //      fmt.Println(userResponse{User: user})
-     mp := make(map[string]interface{})
-     mp ["user"]= user
-  // log in stdout or in stderr
-//     log.Printf("name=%s\n", username)
-//	fmt.Println(string(json_st)) 
-//	fmt.Println(reflect.TypeOf(json))
-//	fmt.Println(userResponse{User: user})	
-// json_st, _ := json.Marshal(mp)
-
-//	fmt.Println("%s\n", json_st)
-        return mp
+	return mp
 }
-
-func calculatePassHash(pass, salt string) string {
-	h := sha1.New()
-	io.WriteString(h, salt)
-	io.WriteString(h, pass)
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-	
-
-
